d313dbc2eed9bd7e7cc836bbb0891e1b2cad03db
Igor <xevius@mail.ru>
2017-12-13 15:24:44 +0200

    some refactoring

--- src/getNews.js
+++ src/getNews.js
@@ -2,7 +2,6 @@ import sendRequest from './sendRequest';
 import renderNews from './news/index';
 
 const getNews = async (source, news) => {
-    console.log(source);
     const articles = await sendRequest(source);
     renderNews(articles, news);
 }
